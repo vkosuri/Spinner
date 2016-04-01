6e0d6a6edb7c68c7dcfb64a9f1542c411c06ab44
Mallikarjunarao Kosuri <malli.kv2@gmail.com>
2016-04-01 17:52:19 +0530

    Fix for broken spinner issue
    Reformatted comments with broken fix

--- spinner.py
+++ spinner.py
@@ -1,17 +1,22 @@
 import re
 import random
 from nltk.corpus import wordnet
+from nltk.corpus import brown
 from nltk.tokenize import regexp_tokenize
 import nltk.data
 from nltk.stem.porter import *
 
-#an example of how to build a simple text spinner using nltk wordnet corpus
-#obviusly you can modify this to work with any other synonym database
-class spinner( object ):
-    
-#     function to spin spintax text using regex    
-#     s = "{Spinning|Re-writing|Rotating|Content spinning|Rewriting} is {fun|enjoyable|entertaining|exciting|enjoyment}! try it {for yourself|on your own|yourself|by yourself|for you} and {see how|observe how|observe} it {works|functions|operates|performs|is effective}."
-#     print spin(s)
+# An example of how to build a simple text spinner using nltk wordnet corpus
+# obviusly you can modify this to work with any other synonym database
+class spinner(object):
+    '''
+    function to spin spintax text using regex
+    s = "{Spinning|Re-writing|Rotating|Content spinning|Rewriting} 
+    is {fun|enjoyable|entertaining|exciting|enjoyment}! try it 
+    {for yourself|on your own|yourself|by yourself|for you} and {
+    see how|observe how|observe} it {works|functions|operates|performs|is effective}."
+    '''
+    # print spin(s)
     def spin(self, s):
         while True:
             s, n = re.subn('{([^{}]*)}',
@@ -19,33 +24,39 @@ class spinner( object ):
                         s)
             if n == 0: break
         return s.strip()
-    
-#   split a paragraph into sentences.
-#   you can use the following replace and split functions or the nltk sentence tokenizer
-#   content = content.replace("\n", ". ")
-#   return content.split(". ")
+
     def splitToSentences(self, content):
+        '''
+        split a paragraph into sentences.
+        you can use the following replace and split functions or the nltk sentence tokenizer
+        content = content.replace("\n", ". ")
+        return content.split(". ")
+        '''
         tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
         return tokenizer.tokenize(content)
-    
-#     get all synonyms of a word from the wordnet database
+
     def getSynonyms(self, word):
-#         include the original word
+        '''
+        Get all synonyms of a word from the wordnet database
+        include the original word
+        '''
         synonyms = [word]
         for syn in wordnet.synsets(word):
-            for lemma in syn.lemmas:
-                if lemma.name != word:
-#                     since wordnet lemma.name will include _ for spaces, we'll replace these with spaces
-                    w, n = re.subn("_", " ", lemma.name)
+            for lemma in syn.lemmas():
+                if lemma.name() != word:
+                    # since wordnet lemma.name will include _ for spaces, we'll replace these with spaces
+                    w, n = re.subn("_", " ", lemma.name())
                     synonyms.append(w)
         s = list(set(synonyms))
         return len(s), s
-    
-#     transform text into spintax with the folowing steps
-#     1. split the text to sentences
-#     2. loop through the sentences and tokenize it
-#     3. loop thorugh each token, find its stem and assemble all the synonyms of it into the spintax
+
     def getSpintax(self, text):
+        '''
+        Transform text into spintax with the folowing steps
+        1. split the text to sentences
+        2. loop through the sentences and tokenize it
+        3. loop thorugh each token, find its stem and assemble all the synonyms of it into the spintax
+        '''
         sentences = self.splitToSentences(text)
         stemmer = PorterStemmer()
         spintax = ""
@@ -72,5 +83,4 @@ if __name__ == '__main__':
     s = spinner()
     spintax = s.getSpintax('Everything in moderation, including moderation.')
     spun = s.spin(spintax)
-    print spintax
-    print spun
+    print spintax, spun
